1fbc3484d740ae34d60391a2ea94b298ec2c71c9
sxf <274997172@qq.com>
2022-06-26 19:34:33 +0800

    提交我的项目

---
+++ mydemovue/src/pages/mytest/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>ddf</div>
+</template>
---
+++ mydemovue/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="setClick()">点击</button>
+    <HomeView v-if="click===2"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  data(){
+    return {
+      click: 1
+    }
+  },
+  methods: {
+    setClick(){
+      this.click++;
+    }
+  },
+  components: {
+    HelloWorld,
+  },
+};
+</script>
